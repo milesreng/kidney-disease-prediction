6173618da04ac34f4580e916a71de035d7106005
Miles Eng <miles.eng@duke.edu>
2023-11-30 10:33:29 -0500

    data type changes

--- test.py
+++ test.py
@@ -13,15 +13,10 @@ from sklearn.metrics import classification_report, confusion_matrix, log_loss, a
 
 warnings.filterwarnings('ignore')
 plt.style.use('fivethirtyeight')
-%matplotlib inline
 pd.set_option('display.max_columns', 26)
 
 data = pd.read_csv('data/chronic_kidney_disease.csv')
-
-data.head()
-
 data.drop('id', axis = 1, inplace = True)
-
 data.columns = ['age',
                 'blood_pressure',
                 'specific_gravity',
@@ -47,3 +42,20 @@ data.columns = ['age',
                 'peda_edema',
                 'anemia',
                 'class']
+
+data['age'] = pd.to_numeric(data['age'], errors='coerce')
+data['blood_pressure'] = pd.to_numeric(data['blood_pressure'], errors='coerce')
+data['specific_gravity'] = pd.to_numeric(data['specific_gravity'], errors='coerce')
+data['albumin'] = pd.to_numeric(data['albumin'], errors='coerce')
+data['sugar'] = pd.to_numeric(data['sugar'], errors='coerce')
+data['blood_glucose_random'] = pd.to_numeric(data['blood_glucose_random'], errors='coerce')
+data['blood_urea'] = pd.to_numeric(data['blood_urea'], errors='coerce')
+data['serum_creatinine'] = pd.to_numeric(data['serum_creatinine'], errors='coerce')
+data['sodium'] = pd.to_numeric(data['sodium'], errors='coerce')
+data['potassium'] = pd.to_numeric(data['potassium'], errors='coerce')
+data['haemoglobin'] = pd.to_numeric(data['haemoglobin'], errors='coerce')
+data['packed_cell_volume'] = pd.to_numeric(data['packed_cell_volume'], errors='coerce')
+data['white_blood_cell_count'] = pd.to_numeric(data['white_blood_cell_count'], errors='coerce')
+data['red_blood_cell_count'] = pd.to_numeric(data['red_blood_cell_count'], errors='coerce')
+
+print(data.info())
